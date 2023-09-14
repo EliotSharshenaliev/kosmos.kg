798b91e2d63c712df7cd7a17179141582eb14165
Marat Sharshenaliev <129174263+EliotSharshenaliev@users.noreply.github.com>
2023-09-15 04:14:39 +0600

    Delete contacts_pages directory

--- contacts_pages/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContactsPagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contacts_pages'
--- contacts_pages/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def contact(request):
-    return render(request=request, template_name="contact.html")
