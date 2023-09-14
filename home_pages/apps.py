51d90fde70273a6a35ba99ce6c4e1b446a0ecf3f
Marat Sharshenaliev <129174263+EliotSharshenaliev@users.noreply.github.com>
2023-09-15 04:15:01 +0600

    Delete home_pages directory

--- home_pages/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomePagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home_pages'
--- home_pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, home2, home3
-
-urlpatterns = [
-    path('home.html', home),
-    path('home-02.html', home2),
-    path('home-03.html', home3),
-]
