a5296ff0426feddf0e67268d201b3cf84c89d3db
Marat Sharshenaliev <129174263+EliotSharshenaliev@users.noreply.github.com>
2023-09-15 04:14:24 +0600

    Delete blog_pages directory

--- blog_pages/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import blog, blog_detail
-
-urlpatterns = [
-    path("blog.html", blog),
-    path("blog-detail.html", blog_detail)
-]
--- blog_pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def blog(request):
-    return render(request=request, template_name="blog.html")
-
-
-def blog_detail(request):
-    return render(request=request, template_name="blog-detail.html")
