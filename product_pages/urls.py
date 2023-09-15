7dd90348f07f18d11e9f11cfa85259c37059e0b1
Marat Sharshenaliev <129174263+EliotSharshenaliev@users.noreply.github.com>
2023-09-15 19:57:58 +0600

    Delete product_pages directory

--- product_pages/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path
-from .views import all_products, shoping_card
-urlpatterns = [
-    path("product.html", all_products),
-    path("shoping-cart.html", shoping_card, name='shoping_cart')
-]
